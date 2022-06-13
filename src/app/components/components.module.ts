0f8cefe4d3e6173b60b1a629ad40bf90d4368ecc
alvaro-lang <alvaropers96@gmail.com>
2022-06-13 13:08:08 +0200

    cambios html

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "@ionic/angular";
-import { TopComponent } from "./top/top.component";
 
 @NgModule({
     declarations:[
-        TopComponent
+        
     ],
     exports: [
-        TopComponent
+        
     ],
     imports: [
         CommonModule,
