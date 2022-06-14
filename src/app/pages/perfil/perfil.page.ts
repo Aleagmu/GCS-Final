6dfd9c3e0ada3e090b720c68f19291d2437d708f
Andrés Felipe López Franco <aflf1@alu.ua.es>
2022-06-14 23:06:25 +0200

    creados input text

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
+  private formData: FormGroup;
+  private readOnly: boolean;
+
   constructor() { }
 
   ngOnInit() {
+      this.readOnly = true;
+      this.formData = new FormGroup({
+        nombre: new FormControl('Francisco López Muñoz'),
+        edad: new FormControl('25'),
+        ciudad: new FormControl('Alicante'),
+        genero: new FormControl('Hombre'),
+        idioma: new FormControl('Español'),
+        descripcion: new FormControl('Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolorum quo nobis culpa doloribus facilis aut laudantium voluptatem, atque dicta, similique et odit totam, cumque consequatur? Ab officia voluptatem itaque.')
+      });
   }
 
 }
