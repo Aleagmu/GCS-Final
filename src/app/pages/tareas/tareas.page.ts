5a8425c431fc8e4af35b728db8d55b854650047d
alvaro-lang <alvaropers96@gmail.com>
2022-06-14 18:38:44 +0200

    pagina tareas terminada

--- src/app/pages/tareas/tareas.page.ts
+++ src/app/pages/tareas/tareas.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @Component({
   selector: 'app-tareas',
@@ -7,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasPage implements OnInit {
 
-  constructor() { }
+  public tareas: any
+
+  constructor() {
+    this.tareas=["tarea1", "tarea2", "tarea3", "tarea4",]
+  }
+
+
+  search(){
+    var valor=(<HTMLInputElement>document.getElementById("input")).value;
+    
+    if (this.tareas.includes(valor.toLocaleLowerCase().replace(" ", ""))){
+      valor = valor.toLocaleLowerCase().replace(" ", "");
+      (<HTMLInputElement>document.getElementById("input")).setAttribute("value", valor);
+      var url = 'tarea/' + valor;
+      window.location.href = url;
+    }
+    else{
+      alert("¡Error! La tarea '" + valor + "' no encontrada")
+    }
+  }
+
 
   ngOnInit() {
   }
