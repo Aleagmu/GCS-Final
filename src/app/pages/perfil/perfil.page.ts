4c33376df73db1d979088d8ecb7da0e989392695
Andrés Felipe López Franco <aflf1@alu.ua.es>
2022-06-16 21:39:30 +0200

    Añadido el modo noche

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -11,12 +11,20 @@ export class PerfilPage implements OnInit {
   private formData: FormGroup;
   private readOnly: boolean;
   private buttonText: string;
+  private darkModeInput: string;
+  private darkModeButtons: string;
+  private darkMode: boolean;
 
   constructor() { }
 
   ngOnInit() {
       this.readOnly = true;
+      this.darkMode = true;
       this.buttonText = 'Modificar';
+
+      this.darkModeButtons = 'dark';
+      this.darkModeInput = 'light';
+
       this.formData = new FormGroup({
         nombre: new FormControl('Francisco López Muñoz'),
         edad: new FormControl('25'),
@@ -32,7 +40,19 @@ export class PerfilPage implements OnInit {
       this.buttonText = 'Modificar';
     }
     else{
-      this.buttonText = 'Confirmar'
+      this.buttonText = 'Confirmar';
     }
+  }
+  switchDarkMode(){
+    this.darkMode = !this.darkMode;
+    if(this.darkMode){
+      this.darkModeButtons = 'dark';
+      this.darkModeInput = 'light';
+    }
+    else{
+      this.darkModeButtons = 'light';
+      this.darkModeInput = 'dark';
+    }
+
   }
 }
