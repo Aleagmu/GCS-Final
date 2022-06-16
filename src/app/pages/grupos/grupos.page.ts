b066bcca1412c0a9cc8b34f233ef1fb10b06e23a
Alejandro Aguado <aam196@alu.ua.es>
2022-06-16 15:18:31 +0200

    Añadidas funcionalidades

--- src/app/pages/grupos/grupos.page.ts
+++ src/app/pages/grupos/grupos.page.ts
@@ -11,13 +11,18 @@ export class GruposPage implements OnInit {
   groups: Group[]
   perteneces: Group[]
 
-  constructor(private router: Router) { this.groups=Groups }
+  constructor(private router: Router) { this.groups=Groups; this.perteneces=[]}
 
   ngOnInit() {
   }
 
   unirseGrupo(group: Group){
     this.perteneces.push(group)
+  }
+
+  salirGrupo(id: number){
+    var filtrado=this.perteneces.filter((item) => item.id !== id)
+    this.perteneces=filtrado
   }
 
 }
